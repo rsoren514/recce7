2f88fcf9e3fd0d705f7bc4efebbcc815c60e8d69
Dina <dsteeve@gmail.com>
2016-04-16 10:50:09 -0600

    change rest services to use query params for the days=1, weeks=1, hours=1, minutes=1, for examples.  Now the rest url will be:  http://localhost:8080/v1/analytics/ports/8023?weeks=5

--- reportserver/server/PortsServiceHandler.py
+++ reportserver/server/PortsServiceHandler.py
@@ -5,31 +5,33 @@ from reportserver.manager import utilities
 
 class PortsServiceHandler():
 
-    def process(self, rqst, tokens):
+    def process(self, rqst, path_tokens, query_tokens):
         uom = None
         units = None
-        print("processing ports request")
+        print("processing ports request:" + str(path_tokens) + str(query_tokens))
 
-        if len(tokens) > 5:
+        if len(query_tokens) > 0:
             try:
-                time_period = utilities.validateTimePeriod(tokens)
+                time_period = utilities.validate_time_period(query_tokens)
                 uom = time_period[0]
                 units = time_period[1]
             except ValueError:
                 rqst.badRequest(units)
+                return
 
-        if len(tokens) >= 5:
-            portNbr = utilities.validatePortNumber(tokens[4])
+        if len(path_tokens) == 5:
+            portNbr = utilities.validate_port_number(path_tokens[4])
             print("requested: " + str(portNbr))
             if portNbr is not None and 0 < portNbr < 9000:
-                self.getPortDataByTime(rqst, portNbr, uom, units)
+                self.get_port_data_by_time(rqst, portNbr, uom, units)
             else:
                 rqst.badRequest()
-
-        if len(tokens) == 4:
+                return
+        elif len(path_tokens) == 4:
             self.get_port_list(rqst)
         else:
             rqst.badRequest()
+            return
 
 
     def get_port_list(self,rqst):
@@ -37,7 +39,7 @@ class PortsServiceHandler():
         jsondata = "{links : [under construction]}"
         rqst.sendJsonResponse(jsondata, 200)
 
-    def getPortDataByTime(self, rqst, portnumber, uom, unit):
+    def get_port_data_by_time(self, rqst, portnumber, uom, unit):
         #default if we aren't given valid uom and unit
         if uom is None or unit is None:
             uom = "days"
